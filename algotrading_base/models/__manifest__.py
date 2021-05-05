41676696b57928765d67155922c05ab3e1f447dc
Oscar Soto <oscar.soto.ochoa@gmail.com>
2021-05-05 15:14:21 +0200

    [FIX] manifest

--- algotrading_base/models/__manifest__.py
+++ algotrading_base/models/__manifest__.py
@@ -18,15 +18,15 @@
 #
 ###############################################################################
 {
-    'name': 'Strategy Golden Cross',
-    'summary': 'Add strategy Golden Cross to scaffolding.',
+    'name': 'Algotrading Base',
+    'summary': 'Algotrading Base.',
     'author': 'Oscar Soto Ochoa',
     'website': 'https://github.com/oscars8a',
     'license': 'LGPL-3',
     'category': 'Trading',
     'version': '14.0.1.0.0',
     'depends': [
-        'algotrading_base',
+        'board',
     ],
     # 'data': [
     # ],
