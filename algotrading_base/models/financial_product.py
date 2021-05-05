aeea33b2cf251e27d9b53872708819a959f7d474
Oscar Soto <oscar.soto.ochoa@gmail.com>
2021-05-05 09:16:36 +0200

    [ADD] LICENSE

--- algotrading_base/models/financial_product.py
+++ algotrading_base/models/financial_product.py
@@ -1,3 +1,6 @@
+###############################################################################
+# For copyright and license notices, see __manifest__.py file in root directory
+###############################################################################
 from odoo import fields, models
 
 
